e2c5a545788429c12f3443d20024a0001a3206d7
HigorRodrigues <higor.italva@gmail.com>
2021-09-04 10:10:36 -0300

    feat(delete): Apaga livro por id

--- src/main/java/com/higor/socialbooks/socialbooks/resources/LivroResource.java
+++ src/main/java/com/higor/socialbooks/socialbooks/resources/LivroResource.java
@@ -33,4 +33,9 @@ public class LivroResource {
     public Livro buscar(@PathVariable("id") Long id){
         return livrosRepository.findById(id).get();
     }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void deletar(@PathVariable("id") Long id){   
+        livrosRepository.deleteById(id);
+    }
 }
