c4e5ec32c744eaa80c96ed3d35f2873a6cd5116c
HigorRodrigues <higor.italva@gmail.com>
2021-09-04 09:41:41 -0300

    feat(livro): adicionando pesquisa por id

--- src/main/java/com/higor/socialbooks/socialbooks/resources/LivroResource.java
+++ src/main/java/com/higor/socialbooks/socialbooks/resources/LivroResource.java
@@ -1,6 +1,7 @@
 package com.higor.socialbooks.socialbooks.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -26,5 +27,10 @@ public class LivroResource {
     @RequestMapping(method = RequestMethod.POST)
     public void salvar(@RequestBody Livro livro){
         livrosRepository.save(livro);
+    }
+
+    @RequestMapping(value= "/{id}", method = RequestMethod.GET)
+    public Livro buscar(@PathVariable("id") Long id){
+        return livrosRepository.findById(id).get();
     }
 }
